0ef95b3285b8b4b2c605c59ecc28302d84943da2
Shraddheya <shraddheya.shrivastava@gamil.com>
2021-06-29 11:40:50 +0530

    code cleanup

--- categorize.py
+++ categorize.py
@@ -1,4 +1,9 @@
+import sys
+import traceback
+from os import path
 # from __future__ import annotations
+
+
 __supportFormat = ".tsv"
 __doc__ = """Usage :
         python categorize.py [filename]
@@ -7,17 +12,15 @@ __doc__ = """Usage :
 
 """.format(__supportFormat)
 
-import sys, traceback
-from os import path
-
 
 def get_file_name() -> str:
     """Get file names from command line arguments"""
     fileName = sys.argv[1:]
     if len(fileName) != 1:
-        raise ValueError("Exactly one (1) Input filename is expected (not given)")
+        raise ValueError("Exactly one Input filename is expected")
 
     return fileName[0]
+
 
 def check_file_valid(fileName: str) -> None:
     """Checks if the fileName provided is valid and exists"""
@@ -25,13 +28,15 @@ def check_file_valid(fileName: str) -> None:
         raise ValueError('No such file "{}" exists'.format(fileName))
 
     if path.splitext(fileName)[-1] != __supportFormat:
-        raise ValueError('Only "{}" files are supported'.format(__supportFormat))
+        raise ValueError('Only "{}" files are supported'
+                         .format(__supportFormat))
 
 
 def get_data(fileName: str) -> list[str]:
     """Read the file and get all data"""
     with open(fileName, 'r') as f:
         return f.readlines()
+
 
 def process_count_categories(allBooks: list[str]) -> dict:
     """Read the file and get all data"""
@@ -44,7 +49,7 @@ def process_count_categories(allBooks: list[str]) -> dict:
                 categories[category] = 0
 
             categories[category] += 1
-            
+
     return categories
 
 
@@ -56,7 +61,7 @@ def main():
         allBooks = get_data(fileName)[1:]
         categories = process_count_categories(allBooks)
         print (categories)
-        
+
     except ValueError as e:
         traceback.print_exc()
         print ("\nRefer to the usage manual\n", __doc__)
@@ -64,4 +69,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
